d9d0496cbd193e8b424b76ba0f38ca1b85103e28
Christopher Larivière <lariviere.c@gmail.com>
2025-03-03 08:15:25 -0500

    feat(structures): parse yaml and json structures in secrets
    - add support for parsing yaml and json structures in bitwarden secrets
    - add unit tests for use cases
    - update readme to reflect the new changes
    - add threeflags, parse_yaml, parse_json and transform_keys to enable the feature
    
    Signed-off-by: Christopher Larivière <lariviere.c@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import * as core from "@actions/core";
 import { BitwardenClient, ClientSettings, DeviceType, LogLevel } from "@bitwarden/sdk-napi";
-import { SecretInput, parseSecretInput } from "./parser";
+import { SecretInput, SecretInputParsed, parseSecretInput, parseSecretParsedInput } from "./parser";
 import { isValidUrl } from "./validators";
+import * as yaml from "js-yaml";
 
 /**
  * The main function for the action.
@@ -12,32 +13,11 @@ export async function run(): Promise<void> {
     core.info("Validating bitwarden/sm-action inputs...");
     const inputs = readInputs();
 
-    core.info("Parsing secrets input");
-    const secretInputs = parseSecretInput(inputs.secrets);
-
-    core.info("Authenticating to Bitwarden");
-    const client = await getBitwardenClient(inputs);
-
-    core.info("Setting Secrets");
-    const secretIds = secretInputs.map((secretInput: SecretInput) => secretInput.id);
-    const secretResponse = await client.secrets().getByIds(secretIds);
-    if (secretResponse.success && secretResponse.data) {
-      const secrets = secretResponse.data.data;
-      secrets.forEach((secret) => {
-        const secretInput = secretInputs.find((secretInput) => secretInput.id === secret.id);
-        if (secretInput) {
-          core.setSecret(secret.value);
-          core.exportVariable(secretInput.outputEnvName, secret.value);
-          core.setOutput(secretInput.outputEnvName, secret.value);
-        }
-      });
+    if (inputs.parseYaml || inputs.parseJson) {
+      await multiParse(inputs);
     } else {
-      throw Error(
-        `The secrets provided could not be found. Please check the machine account has access to the secret UUIDs provided.\nError: ${secretResponse.errorMessage}`,
-      );
+      await normalParse(inputs);
     }
-
-    core.info("Completed setting secrets as environment variables.");
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message);
@@ -50,6 +30,9 @@ type Inputs = {
   secrets: string[];
   identityUrl: string;
   apiUrl: string;
+  parseYaml: boolean;
+  parseJson: boolean;
+  transformKeys: boolean;
 };
 
 function readInputs(): Inputs {
@@ -63,6 +46,18 @@ function readInputs(): Inputs {
   const baseUrl: string = core.getInput("base_url");
   let identityUrl: string = core.getInput("identity_url");
   let apiUrl: string = core.getInput("api_url");
+
+  const parseYamlInput = core.getInput("parse_yaml");
+  const parseJsonInput = core.getInput("parse_json");
+  const transformKeysInput = core.getInput("transform_keys");
+
+  const parseYaml = parseYamlInput === "" ? false : core.getBooleanInput("parse_yaml");
+  const parseJson = parseJsonInput === "" ? false : core.getBooleanInput("parse_json");
+  const transformKeys = transformKeysInput === "" ? true : core.getBooleanInput("transform_keys");
+
+  if (parseYaml && parseJson) {
+    throw new TypeError("parse_yaml and parse_json cannot both be set to true");
+  }
 
   let customUrls = false;
   //Check if only identityUrl is set and not apiUrl: if so throw an error, otherwise mark this as the user is using customUrls
@@ -118,6 +113,9 @@ function readInputs(): Inputs {
     secrets,
     identityUrl,
     apiUrl,
+    parseYaml,
+    parseJson,
+    transformKeys,
   };
 }
 
@@ -136,4 +134,106 @@ async function getBitwardenClient(inputs: Inputs): Promise<BitwardenClient> {
   }
 
   return client;
+}
+
+async function normalParse(inputs: Inputs) {
+  core.info("Parsing secrets input");
+  const secretInputs = parseSecretInput(inputs.secrets);
+
+  core.info("Authenticating to Bitwarden");
+  const client = await getBitwardenClient(inputs);
+
+  core.info("Setting Secrets");
+  const secretIds = secretInputs.map((secretInput: SecretInput) => secretInput.id);
+  const secretResponse = await client.secrets().getByIds(secretIds);
+  if (secretResponse.success && secretResponse.data) {
+    const secrets = secretResponse.data.data;
+    secrets.forEach((secret) => {
+      const secretInput = secretInputs.find((secretInput) => secretInput.id === secret.id);
+      if (secretInput) {
+        core.setSecret(secret.value);
+        core.exportVariable(secretInput.outputEnvName, secret.value);
+        core.setOutput(secretInput.outputEnvName, secret.value);
+      }
+    });
+  } else {
+    throw Error(
+      `The secrets provided could not be found. Please check the machine account has access to the secret UUIDs provided.\nError: ${secretResponse.errorMessage}`,
+    );
+  }
+
+  core.info("Completed setting secrets as environment variables.");
+}
+
+async function multiParse(inputs: Inputs) {
+  core.info("Parsing secrets input");
+  const secretInputs = parseSecretParsedInput(inputs.secrets);
+
+  core.info("Authenticating to Bitwarden");
+  const client = await getBitwardenClient(inputs);
+
+  core.info("Setting Secrets");
+  const secretIds = secretInputs.map((secretInput: SecretInputParsed) => secretInput.id);
+  const secretResponse = await client.secrets().getByIds(secretIds);
+  if (secretResponse.success && secretResponse.data) {
+    const secrets = secretResponse.data.data;
+    secrets.forEach((secret) => {
+      const secretInput = secretInputs.find((secretInput) => secretInput.id === secret.id);
+      if (!secretInput) {
+        return;
+      }
+
+      if (inputs.parseJson || inputs.parseYaml) {
+        let parsedObj: Record<string, unknown>;
+        try {
+          if (inputs.parseJson) {
+            parsedObj = parseJsonWithDuplicatesCheck(secret.value);
+          } else {
+            parsedObj = yaml.load(secret.value) as Record<string, unknown>;
+          }
+        } catch (error: any) {
+          throw new Error(`Failed to parse secret '${secretInput}': ${error.message}`);
+        }
+
+        Object.entries(parsedObj).forEach(([key, value]) => {
+          if (typeof value === "object" && value !== null) {
+            throw new Error(
+              `Secret contains nested structures. Only flat key-value pairs are allowed: '${key}'`,
+            );
+          }
+
+          if (inputs.transformKeys) {
+            key = key.toUpperCase();
+          }
+
+          const valueStr = typeof value === "string" ? value : JSON.stringify(value);
+          core.setSecret(valueStr);
+          core.exportVariable(key, valueStr);
+          core.setOutput(key, valueStr);
+        });
+      }
+    });
+  } else {
+    throw Error(
+      `The secrets provided could not be found. Please check the machine account has access to the secret UUIDs provided.\nError: ${secretResponse.errorMessage}`,
+    );
+  }
+
+  core.info("Completed setting secrets as environment variables.");
+}
+
+// the json library automatically overwrites the last key with the value it saw.
+// this function checks for duplicates and throws an error if it finds any.
+function parseJsonWithDuplicatesCheck(jsonString: string): Record<string, unknown> {
+  const seenKeys = new Set<string>();
+
+  jsonString.replace(/"([^"]+)":/g, (_, key) => {
+    if (seenKeys.has(key)) {
+      throw new Error(`Duplicate key detected: '${key}'. Secrets must have unique keys.`);
+    }
+    seenKeys.add(key);
+    return key;
+  });
+
+  return JSON.parse(jsonString);
 }
